09ae793592b5cf6d9ad143e26dd42b8eaa7d016a
LeviMonsha <lev.coposow@yandex.ru>
2026-03-12 15:35:28 +0300

    [main] add new notebooks #base-1-2

--- test_lib/spark/main.py
+++ test_lib/spark/main.py
@@ -3,7 +3,7 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 schema = StructType(fields=[
     StructField("col_1", IntegerType()),
-    StructField("col_2", StringType ())
+    StructField("col_2", StringType())
 ])
 
 df = spark.read.csv("/path/file_name", schema=schema, sep=";")
